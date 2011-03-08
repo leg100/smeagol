f6842e36ee58d32b147ac934406c0fb4bf4cbe74
Louis Garman <louisgarman@gmail.com>
2011-03-08 17:44:37 +0000

    This task takes a while

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -49,16 +49,16 @@ template "#{ENV['HOME']}/.rvm/gemsets/default.gems" do
   source "default.gems.erb"
 end
 
-script "ensuring default rubygems are installed" do
-  interpreter "bash"
-  code <<-EOS
-    source ~/.cinderella.profile
-    rvm gemset load ~/.rvm/gemsets/default.gems >> ~/.cinderella/ruby.log 2>&1
-  EOS
-end
+# script "ensuring default rubygems are installed" do
+#   interpreter "bash"
+#   code <<-EOS
+#     source ~/.cinderella.profile
+#     rvm gemset load ~/.rvm/gemsets/default.gems >> ~/.cinderella/ruby.log 2>&1
+#   EOS
+# end
 
 execute "cleanup rvm build artifacts" do
-  command "find ~/Developer/.rvm/src -depth 1 | grep -v src/rvm | xargs rm -rf "
+  command "find ~/.rvm/src -depth 1 | grep -v src/rvm | xargs rm -rf "
 end
 
 template "#{ENV['HOME']}/.gemrc" do
